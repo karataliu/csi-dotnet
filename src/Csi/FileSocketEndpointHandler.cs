a3dc74f9b5eb66759dd11bff875e1979a185517b
Dong Liu <doliu@microsoft.com>
2018-04-28 14:32:40 +0800

    Add file socket handler

---
+++ src/Csi/FileSocketEndpointHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Logging;
+
+namespace Csi
+{
+    class FileSocketEndpointHandler : IDisposable
+    {
+        private const string host = "127.0.0.1";
+        private const int port = 10000;
+        private const string scBIn = "socat";
+        private ILogger logger = GrpcEnvironment.Logger;
+        private Process process;
+
+        public ServerPort Handle(string socket)
+        {
+            var processInfo = new ProcessStartInfo(scBIn)
+            {
+                Arguments = $"-d UNIX-LISTEN:{socket},fork TCP4:{host}:{port}",
+                RedirectStandardOutput = false,
+            };
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        process = Process.Start(processInfo);
+                        process.WaitForExit();
+                        logger.Warning("process exits, will restart.");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Warning("exception " + ex.Message);
+                    }
+                    Thread.Sleep(2000);
+                }
+            });
+
+            return new ServerPort(host, port, ServerCredentials.Insecure);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                logger.Info("Stopping process");
+                process?.Kill();
+                disposedValue = true;
+            }
+        }
+        #endregion
+
+    }
+}
